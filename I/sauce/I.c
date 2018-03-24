67dc8f5212c3cc02271316f7c522e71b81196b3f
simon00krueger <webspace20@gmx.de>
2018-03-24 12:59:44 +0100

    Add first licenseCheck

--- I/sauce/I.c
+++ I/sauce/I.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-	
+int main( int argc, char *argv[] ) {
+	if (argc < 2) {
+		printf("Usage: %s <key>\n", argv[0]);
+	} else {
+		if (strlen(argv[1]) == 10) {
+			printf("Key correct!\n");
+		} else {
+			printf("Thats the wrooong key.\n");
+		}
+	}
+	return 0;
 }
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Reverse C Ingwer flip
 
+## Usage
+e.g. 1st licenseCheck:
+I
+|-sauce (<-- source code for checking solution)
+ -I (<-- the compiled version of the sauce code; to be reversed)
+
+Goal: write keygen that spits out a working key for the respective licenseCheck
+
+## Tools
 - Radare2
 - strings
+- ...
